1d7adc06d8904b935b65f74aee61150d8145dd64
Sgedelta <Sgeaston04@gmail.com>
2024-10-18 01:20:33 -0400

    added server code - no endpoints yet

--- src/server.js
+++ src/server.js
@@ -1,9 +1,50 @@
 const http = require('http');
+const query = require('querystring');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = (request, response) => {
+const getUrlStruct = { };
 
+const postUrlStruct = { };
+
+const parseBody = (request, response, handler) => {
+    const body = [];
+
+    request.on('error', (err) => {
+        console.dir(err);
+        response.statusCode = 400;
+        response.end();
+    });
+
+    request.on('data', (chunk) => {
+        body.push(chunk);
+    });
+
+    request.on('end', () => {
+        const bodyString = Buffer.concat(body).toString();
+        request.body = query.parse(bodyString);
+        handler(request, response);
+    });
+};
+
+const onRequest = (request, response) => {
+    const protocol = request.connection.encrypted ? 'https' : 'http';
+    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
+
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.acceptedTypes = request.headers.accept.split(',');
+
+    if (request.method === 'POST') {
+        if (postUrlStruct[parsedUrl.pathname]) {
+            parseBody(request, response, postUrlStruct[parsedUrl.pathname]);
+        } else {
+            getUrlStruct[404](request, response);
+        }
+    } else if (getUrlStruct[parsedUrl.pathname]) {
+        getUrlStruct[parsedUrl.pathname](request, response);
+    } else {
+        getUrlStruct[404](request, response);
+    }
 };
 
 http.createServer(onRequest).listen(port, () => {
